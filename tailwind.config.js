c915f7cade284a394054ead5e2a0ae7b1328b3f6
shervan <shervangh@gmail.com>
2022-08-18 02:26:21 +0430

    git page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bgblue': '#0a192f',
+      },
+    },
+  },
+  plugins: [],
+}
